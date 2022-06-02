efa7d87cb5fd67fbe14f1de8174d1c38f49ab845
Ayomiposi <perscey007@gmail.comm>
2022-06-02 09:49:02 +0100

    js moved to file

---
+++ main.js
@@ -0,0 +1,124 @@
+ var a;
+
+          function myFunction(){
+          if(a==1){
+          document.getElementById("M-nav-ham").style.display="none";
+
+      
+          return a=0;
+      }
+          else {
+          document.getElementById("M-nav-ham").style.display="block";
+
+        
+          return a=1;
+    }
+}
+
+  var a1;
+
+          function myNav(){
+          if(a1==1){
+          document.getElementById("id-nav-background1").style.display="none";
+
+      
+          return a1=0;
+      }
+          else {
+          document.getElementById("id-nav-background1").style.display="block";
+
+        
+          return a1=1;
+    }
+}
+
+
+  var a2;
+
+          function myNav2(){
+          if(a2==1){
+          document.getElementById("id-nav-background2").style.display="none";
+
+      
+          return a2=0;
+      }
+          else {
+          document.getElementById("id-nav-background2").style.display="block";
+
+        
+          return a2=1;
+    }
+}
+
+
+  var a3;
+
+          function myNav3(){
+          if(a3==1){
+          document.getElementById("id-nav-background3").style.display="none";
+
+      
+          return a3=0;
+      }
+          else {
+          document.getElementById("id-nav-background3").style.display="block";
+
+        
+          return a3=1;
+    }
+}
+
+
+
+  var n1;
+
+          function myNavPc1(){
+          if(n1==1){
+          document.getElementById("nav-dropdown1").style.display="none";
+
+      
+          return n1=0;
+      }
+          else {
+          document.getElementById("nav-dropdown1").style.display="block";
+
+        
+          return n1=1;
+    }
+}
+
+
+  var n2;
+
+          function myNavPc2(){
+          if(n2==1){
+          document.getElementById("nav-dropdown2").style.display="none";
+
+      
+          return n2=0;
+      }
+          else {
+          document.getElementById("nav-dropdown2").style.display="block";
+
+        
+          return n2=1;
+    }
+}
+
+
+  var n3;
+
+          function myNavPc3(){
+          if(n3==1){
+          document.getElementById("nav-dropdown3").style.display="none";
+
+      
+          return n3=0;
+      }
+          else {
+          document.getElementById("nav-dropdown3").style.display="block";
+
+        
+          return n3=1;
+    }
+}
